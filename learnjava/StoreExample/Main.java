1a22d76e02b0f48487b99b258b56f3a64a874f19
Romina French <108244092+rbf123@users.noreply.github.com>
2024-06-16 18:34:32 -0500

    Create Main.java

---
+++ learnjava/StoreExample/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    Store lemonadeStand = new Store("Lemonade", 3.75);
+    Store cookieShop = new Store("Cookies", 5);
+
+    System.out.println(lemonadeStand);
+    System.out.println(cookieShop);
+  }
+}
