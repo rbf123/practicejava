74447a3309b35a1f32cdcb49d150ab952c6d0c00
Romina French <108244092+rbf123@users.noreply.github.com>
2024-06-16 14:02:37 -0500

    Create Tree.java
    - Printing out a tree to the screen using Java

---
+++ learnjava/Tree.java
@@ -0,0 +1,16 @@
+public class Tree{
+  public static void main(String[] args) {
+    //We're going to introduce outselves and print a tree to the screen
+    System.out.println("Hey there, I'm Romina and I'm learning to code in Java!");
+    System.out.println("Ready to plant a tree today");
+    System.out.println("    *  ");
+    System.out.println("   *** ");
+    System.out.println("  ***** ");
+    System.out.println(" ******* ");
+    System.out.println("  ***** ");
+    System.out.println("   *** ");
+    System.out.println("    *  ");
+    System.out.println("    *  ");
+    System.out.println("~~~~~~~~~~");
+  }
+}
